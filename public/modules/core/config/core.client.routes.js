0a2638982b9c9acbd4bbeb9ef1c8c7bc2e2a41aa
Koding <crowdsourcecreativity@gmail.com>
2015-06-02 02:46:46 +0000

    updated css

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -6,12 +6,11 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		// Redirect to home view when route not found
 		$urlRouterProvider.otherwise('/');
 
-		/* Home state routing
 		$stateProvider.
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
 		});
-		*/
+		
 	}
 ]);
